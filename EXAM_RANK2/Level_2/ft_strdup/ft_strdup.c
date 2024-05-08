ce2a8dcb735807bf5d8d438688ffe685d7e94946
Lalla-Asma Aichouni <laichoun@c1r5s5.42luxembourg.lu>
2024-05-08 09:38:47 +0200

    update add subject

--- EXAM_RANK2/Level_2/ft_strdup/ft_strdup.c
+++ EXAM_RANK2/Level_2/ft_strdup/ft_strdup.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: laichoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 13:31:53 by laichoun          #+#    #+#             */
-/*   Updated: 2024/04/16 13:51:43 by laichoun         ###   ########.fr       */
+/*   Created: 2024/04/26 12:32:10 by laichoun          #+#    #+#             */
+/*   Updated: 2024/04/26 12:39:07 by laichoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 
 char	*ft_strdup(char *src)
 {
-	int		i;
 	char	*duplicate;
-	int		len;
+	int		i;
 
 	i = 0;
-	len = 0;
-	while (src[len])
-		len ++;
-	duplicate = malloc((len + 1) * sizeof(char));
+	while (src[i])
+		i ++;
+	duplicate = malloc ((i + 1) * sizeof(char));
 	if (duplicate == NULL)
 		return (NULL);
+	i = 0;
 	while (src[i])
 	{
 		duplicate[i] = src[i];
@@ -37,7 +36,7 @@ char	*ft_strdup(char *src)
 
 int	main(void)
 {
-	char	src[100] = "Monkey D Luffy";
+	char	src[100] = "Bonjour Asma";
 	char	*duplicate;
 
 	duplicate = ft_strdup(src);
