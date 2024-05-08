062f52df8133361f08ec0e86b829c732a2b208ac
Lalla-Asma Aichouni <laichoun@c1r5s5.42luxembourg.lu>
2024-05-08 09:58:09 +0200

    update add subject + prgm

---
+++ EXAM_RANK2/Level_4/rev_wstr/rev_wstr.c
@@ -0,0 +1,43 @@
+
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	start;
+	int	end;
+	int	flag;
+
+	i = 0;
+	flag = 0;
+	start = 0;
+	end = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+			i ++;
+		while (i >= 0)
+		{
+			while (argv[1][i] == '\0' || argv[1][i] == 32 || argv[1][i] == '\t')
+				i --;
+			end = i;
+			while (argv[1][i] != '\0' && argv[1][i] != 32 && argv[1][i] != '\t' && argv[1][i])
+			i --;
+			start = i + 1;
+			flag = start;
+			while (start <= end)
+			{
+				write(1, &argv[1][start], 1);
+				start ++;
+			}
+			if (flag != 0)
+			{
+				write(1, " ", 1);
+				flag = 0;
+			}
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
