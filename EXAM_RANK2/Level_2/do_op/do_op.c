dba5337ac796499196a150b1c35f73e323df2166
Lalla-Asma Aichouni <laichoun@c1r5s5.42luxembourg.lu>
2024-05-08 09:36:34 +0200

    update add subject

--- EXAM_RANK2/Level_2/do_op/do_op.c
+++ EXAM_RANK2/Level_2/do_op/do_op.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: laichoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 08:31:01 by laichoun          #+#    #+#             */
-/*   Updated: 2024/04/16 08:49:04 by laichoun         ###   ########.fr       */
+/*   Created: 2024/04/25 15:48:09 by laichoun          #+#    #+#             */
+/*   Updated: 2024/04/26 11:23:37 by laichoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,16 @@ int	main(int argc, char **argv)
 	if (argc == 4)
 	{
 		if (argv[2][0] == '+')
-			printf("%d\n", atoi(argv[1]) + atoi(argv[3]));
+			printf("%d", atoi(argv[1]) + atoi(argv[3]));
 		else if (argv[2][0] == '-')
-			printf("%d\n", atoi(argv[1]) - atoi(argv[3]));
+			printf("%d", atoi(argv[1]) - atoi(argv[3]));
 		else if (argv[2][0] == '*')
-			printf("%d\n", atoi(argv[1]) * atoi(argv[3]));
+			printf("%d", atoi(argv[1]) * atoi(argv[3]));
 		else if (argv[2][0] == '/')
-			printf("%d\n", atoi(argv[1]) / atoi(argv[3]));
-		else if (argv[2][0] == '/')
-			printf("%d\n", atoi(argv[1]) % atoi(argv[3]));
+			printf("%d", atoi(argv[1]) / atoi(argv[3]));
+		else if (argv[2][0] == '%')
+			printf("%d", atoi(argv[1]) % atoi(argv[3]));
 	}
-	else
-		write(1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
