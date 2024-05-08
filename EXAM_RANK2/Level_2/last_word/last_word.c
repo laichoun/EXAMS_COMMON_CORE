1dd88c2bcfc0c83b308fb7748076e3f5d74b1bd9
Lalla-Asma Aichouni <laichoun@c1r5s5.42luxembourg.lu>
2024-05-08 09:41:53 +0200

    update add subject

--- EXAM_RANK2/Level_2/last_word/last_word.c
+++ EXAM_RANK2/Level_2/last_word/last_word.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: laichoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/17 08:48:00 by laichoun          #+#    #+#             */
-/*   Updated: 2024/04/17 09:07:18 by laichoun         ###   ########.fr       */
+/*   Created: 2024/04/26 13:41:09 by laichoun          #+#    #+#             */
+/*   Updated: 2024/04/26 13:53:45 by laichoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char **argv)
 {
 	int		i;
-	char	*lw;
+	char *lw;
 
 	i = 0;
 	if (argc == 2)
@@ -23,9 +23,7 @@ int	main(int argc, char **argv)
 		while (argv[1][i])
 		{
 			if (argv[1][i] <= 32 && argv[1][i + 1] > 32)
-			{
 				lw = &argv[1][i + 1];
-			}
 			i ++;
 		}
 		i = 0;
@@ -36,4 +34,30 @@ int	main(int argc, char **argv)
 		}
 	}
 	write(1, "\n", 1);
+	return (0);
 }
+
+
+/*
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+			i ++;
+		while (argv[1][i] != 32 && argv[1][i] != '\t')
+			i --;
+		i ++;
+		while (argv[1][i])
+		{
+			write(1, &argv[1][i], 1);
+			i ++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+*/
