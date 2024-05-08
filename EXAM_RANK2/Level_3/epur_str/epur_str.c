1acae1af736af5f1eabb81367470d0118bcef12f
Lalla-Asma Aichouni <laichoun@c1r5s5.42luxembourg.lu>
2024-05-08 09:49:26 +0200

    update add subject + prgm

---
+++ EXAM_RANK2/Level_3/epur_str/epur_str.c
@@ -0,0 +1,31 @@
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	space;
+	int	i;
+
+	i = 0;
+	space = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i] == 32 || (argv[1][i] >= 9 && argv[1][i] <= 13))
+			i ++;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == 32 || (argv[1][i] >= 9 && argv[1][i] <= 13))
+				space = 1;
+			if (!(argv[1][i] == 32 || (argv[1][i] >= 9 && argv[1][i] <= 13)))
+			{
+				if (space)
+					write(1, " ", 1);
+				write(1, &argv[1][i], 1);
+				space = 0;
+			}
+			i ++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
