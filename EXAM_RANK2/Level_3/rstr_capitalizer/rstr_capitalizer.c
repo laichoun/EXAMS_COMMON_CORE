6ffdc6517b7c77f8aec3c07147f7e968f9bd9f07
Lalla-Asma Aichouni <laichoun@c1r5s5.42luxembourg.lu>
2024-05-08 09:54:31 +0200

    update add subject + prgm

---
+++ EXAM_RANK2/Level_3/rstr_capitalizer/rstr_capitalizer.c
@@ -0,0 +1,38 @@
+
+
+#include <unistd.h>
+
+void	rstr_capitalizer(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == 32 || str[i + 1] == '\0' || str[i + 1] == '\t'))
+			str[i] = str[i] - 32;
+		write(1, &str[i], 1);
+		i ++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i <= argc - 1)
+		{
+			rstr_capitalizer(argv[i]);
+			write(1, "\n", 1);
+			i ++;
+		}
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
+}
