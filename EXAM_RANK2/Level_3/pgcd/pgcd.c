3f8dfb8d3309382b81b32b4cdbac8af3bbb47b75
laichoun <167307369+laichoun@users.noreply.github.com>
2024-09-16 15:10:48 +0200

    Update pgcd.c

--- EXAM_RANK2/Level_3/pgcd/pgcd.c
+++ EXAM_RANK2/Level_3/pgcd/pgcd.c
@@ -27,3 +27,32 @@ int	main(int argc, char **argv)
 	printf("\n");
 	return (0);
 }
+
+
+/*autre version : pibernar*/
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	n1;
+	int	n2;
+	int	pgcd;
+
+	i = 0;
+	if (argc != 3)
+	{
+		printf("\n");
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	if (n1 > 0 && n2 > 0)
+	{
+		while (i <= n1 / 2 && i <= n2 /2)
+		{
+			if (n1 % i == 0 && n2 % i == 0)
+				pgcd = i;
+			i ++;
+		}
+	}
+	return (0);
+}
